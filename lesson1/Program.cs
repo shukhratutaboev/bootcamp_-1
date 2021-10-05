23a7701c576d6fe59050448ae061d1bd5d871a28
shukhratutaboev <shuhratutaboyev2597@gmail.com>
2021-10-05 20:23:22 +0500

    Initial commit

---
+++ lesson1/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Linq;
+
+namespace lesson1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // string name = Console.ReadLine();
+            // Console.WriteLine("Welcome, " + name + ".");
+            // Console.WriteLine(float.MaxValue.ToString() + " " + int.MinValue.ToString());
+
+            var inputs = Console.ReadLine().Split(' ', StringSplitOptions.RemoveEmptyEntries);
+            // foreach(string input in inputs)
+            // {
+            //     int son = 0;
+            //     bool parsed = int.TryParse(input, out son);
+            //     if(parsed)
+            //     {
+            //         System.Console.WriteLine(son);
+            //     }
+            //     else
+            //     {
+            //         System.Console.WriteLine("Not integer");
+            //     }
+                
+            // }
+            var ints = inputs.Select(i => 
+            {
+                int son = int.Parse(i);
+                Console.WriteLine(son);
+                return son;
+            }).ToList();
+        }
+    }
+}
